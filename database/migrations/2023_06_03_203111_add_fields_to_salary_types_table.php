9c4f3c63c5d9cab693ceba8e29ec66be1fc01144
Admin <admin@AlanPacheco.local>
2023-06-20 23:54:07 -0700

    hola

--- database/migrations/2023_06_03_203111_add_fields_to_salary_types_table.php
+++ database/migrations/2023_06_03_203111_add_fields_to_salary_types_table.php
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropColumns('salary_types', ['type']);
+        Schema::dropColumns('salary_types', ['title']);
     }
 };
