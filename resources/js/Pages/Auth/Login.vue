64c58cd9717bbfcbd4efc27bd4b62ea1d94161d8
Admin <admin@AlanPacheco.local>
2023-07-30 19:00:24 -0700

    Se dejo solo inicio con google

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -103,29 +103,6 @@
                         </span>
                         <span>Iniciar sesión con Google</span>
                     </a>
-                    <a
-                        href="/auth/facebook/redirect"
-                        class="flex items-center justify-center hover:bg-gray-900 transition-all md:text-sm duration-300 ease-in-out font-semibold bg-black flex-none px-3 py-3 md:px-4 rounded-md text-white border-gray-300 relative"
-                    >
-                        <span class="absolute left-4">
-                            <svg
-                                class="w-5 h-5 mr-2 -ml-1"
-                                aria-hidden="true"
-                                focusable="false"
-                                data-prefix="fab"
-                                data-icon="apple"
-                                role="img"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 384 512"
-                            >
-                                <path
-                                    fill="currentColor"
-                                    d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
-                                ></path>
-                            </svg>
-                        </span>
-                        <span>Iniciar sesión con Apple</span>
-                    </a>
                 </div>
                 <div class="mb-4">
                     <p class="text-gray-400 text-sm">O con tu email</p>
