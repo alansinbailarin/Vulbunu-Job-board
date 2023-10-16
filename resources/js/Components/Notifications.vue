0be039138b830c40feb7ccc387932a001ba9649c
Admin <admin@AlanPacheco.local>
2023-10-16 16:30:28 -0700

    Limited the numbers of notifications

--- resources/js/Components/Notifications.vue
+++ resources/js/Components/Notifications.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="rounded-md text-sm bg-white border border-gray-200"
+        class="rounded-md text-sm bg-white border max-h-[calc(100vh-8rem)] border-gray-200"
         role="menu"
         aria-orientation="vertical"
         aria-labelledby="menu-button"
@@ -8,7 +8,7 @@
     >
         <div
             v-if="props.user?.notifications?.length > 0"
-            v-for="notification in props.user.notifications"
+            v-for="notification in notifications"
             :key="notification.id"
         >
             <div
@@ -33,8 +33,8 @@
         </div>
         <div v-else>
             <div class="px-4 py-2">
-                <h1 class="text-sm font-bold text-gray-700">We sorry</h1>
-                <p class="text-sm leading-5 text-gray-600">
+                <h1 class="text-sm font-semibold text-gray-600">We sorry</h1>
+                <p class="text-sm leading-5 text-gray-500">
                     There seems to be no notification here
                 </p>
             </div>
@@ -45,11 +45,17 @@
 <script setup>
 import { Link, router } from "@inertiajs/vue3";
 import moment from "moment";
+import { computed } from "vue";
 
 moment.locale("en");
+let notificationCount = 0;
 
 const props = defineProps({
     user: Object,
+});
+
+const notifications = computed((count) => {
+    return props.user?.notifications.slice(0, count);
 });
 
 const markNotificationAsReaded = (id) => {
