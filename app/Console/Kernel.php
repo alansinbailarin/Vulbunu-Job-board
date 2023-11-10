6c22a9185b1329ac2b70ad589c28667e244b165e
Admin <admin@AlanPacheco.local>
2023-11-10 14:47:12 -0800

    Changed from minutely to weekly to the command job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command(ChangeJobStatus::class)->daily();
-        $schedule->command(SendJobsToCustomers::class)->everyMinute();
+        $schedule->command(SendJobsToCustomers::class)->weekly();
     }
 
     /**
