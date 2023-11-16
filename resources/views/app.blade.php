14eb2f52036530570f39e764378b2486d2f80cbc
Admin <admin@AlanPacheco.local>
2023-11-15 16:43:51 -0800

    Changed thumnail image

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,31 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- HTML Meta Tags -->
+    <title>Vulbunu - Find the best offers that fit your needs</title>
+    <meta name="description"
+        content="Search our vast list of jobs published daily and available talents, find the one that best suits you">
+
+    <!-- Facebook Meta Tags -->
+    <meta property="og:url" content="https://www.vulbunu.com">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Vulbunu - Find the best offers that fit your needs">
+    <meta property="og:description"
+        content="Search our vast list of jobs published daily and available talents, find the one that best suits you">
+    <meta property="og:image" content="https://i.postimg.cc/nrkkj9HX/imgprom.jpg">
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="vulbunu.com">
+    <meta property="twitter:url" content="https://www.vulbunu.com">
+    <meta name="twitter:title" content="Vulbunu - Find the best offers that fit your needs">
+    <meta name="twitter:description"
+        content="Search our vast list of jobs published daily and available talents, find the one that best suits you">
+    <meta name="twitter:image" content="https://i.postimg.cc/nrkkj9HX/imgprom.jpg">
+
+    <!-- Meta Tags Generated via https://www.opengraph.xyz -->
+
     <script src="https://www.paypal.com/sdk/js?client-id={{ config('services.paypal.client_id') }}&currency=USD"
         data-sdk-integration-source="button-factory" data-namespace="paypal_sdk"></script>
 
