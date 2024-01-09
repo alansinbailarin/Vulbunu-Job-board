a0c28a12e82b897fafc926df158816ceae2b3fed
Admin <admin@AlanPacheco.local>
2024-01-08 22:40:17 -0800

    asd

--- resources/js/Pages/Jobs/Create.vue
+++ resources/js/Pages/Jobs/Create.vue
@@ -1077,7 +1077,7 @@
                         class="w-4 mt-0.5 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
                         v-model="form.featured"
                     />
-                    <label for="featured">Publish you job for free</label>
+                    <label for="featured"> Publish you job for free</label>
                 </div>
                 <!-- <div
                     id="paypal-button-container"
