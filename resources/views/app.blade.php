38255305242a6b867fd893ebc2f36d55546e94df
Admin <admin@AlanPacheco.local>
2024-01-09 13:21:17 -0800

    Added google tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,6 +36,18 @@
     @vite('resources/js/app.js')
     @inertiaHead
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-H2JLXWGRTH"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-H2JLXWGRTH');
+</script>
 
 <body class="text-gray-800 mb-0">
     @inertia
